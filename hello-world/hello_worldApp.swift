2c5f33db480800d788d0702c19ff3ad640cf80f0
Jason Leyanna <jasonleyanna@Jasons-MBP.home>
2023-07-02 19:14:38 -0400

    first commit

---
+++ hello-world/hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  hello-world
+//
+//  Created by Jason Leyanna on 7/2/23.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
